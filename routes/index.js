bca41131c8639fa84522a2fe19d662e3abedce0a
Daniel A.C. Martin <daniel-ac-martin@users.noreply.github.com>
2015-08-26 17:56:51 +0100

    Update dependencies

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@
 var
   wizard     = require('hmpo-form-wizard'),
   mixins     = require('hmpo-template-mixins'),
-  i18n       = require('i18n-future')(),
+  i18n       = require('i18n-future'),
   router     = require('express').Router(),
   formSteps  = require('./steps'),
   fields     = require('./fields'),
-  formWizard = wizard(formSteps, fields, { translate: i18n.translate.bind(i18n) });
+  formWizard = wizard(formSteps, fields, { translate: i18n().translate.bind(i18n) });
 
-router.use(mixins(i18n.translate.bind(i18n), fields));
+router.use(i18n.middleware());
+router.use(mixins(fields));
 
 router.get ('/',        require('../controllers/index'));
 router.use('/form/',    formWizard);
